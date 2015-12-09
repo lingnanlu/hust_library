867d3364a05fca5a5a9df51f55c1c472d01605f0
lingnanlu <lingnanlu@gmail.com>
2015-12-09 12:13:42 +0800

    hehe

---
+++ app/src/main/java/io/github/lingnanlu/hustlibrary/Book.java
@@ -0,0 +1,84 @@
+package io.github.lingnanlu.hustlibrary;
+
+/**
+ * Created by Rico on 2015/12/9.
+ */
+public class Book {
+
+    private String mTitle;
+    private String mAuthor;
+    private String mTranslator;
+    private String mPress;
+    private String mAbstracts;
+    private String mISBN;
+
+    public String getmCallNumber() {
+        return mCallNumber;
+    }
+
+    public void setmCallNumber(String mCallNumber) {
+        this.mCallNumber = mCallNumber;
+    }
+
+    public String getmISBN() {
+        return mISBN;
+    }
+
+    public void setmISBN(String mISBN) {
+        this.mISBN = mISBN;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public void setmLocation(String mLocation) {
+        this.mLocation = mLocation;
+    }
+
+    public String getmAbstracts() {
+        return mAbstracts;
+    }
+
+    public void setmAbstracts(String mAbstracts) {
+        this.mAbstracts = mAbstracts;
+    }
+
+    public String getmPress() {
+        return mPress;
+    }
+
+    public void setmPress(String mPress) {
+        this.mPress = mPress;
+    }
+
+    public String getmTranslator() {
+        return mTranslator;
+    }
+
+    public void setmTranslator(String mTranslator) {
+        this.mTranslator = mTranslator;
+    }
+
+    public String getmAuthor() {
+        return mAuthor;
+    }
+
+    public void setmAuthor(String mAuthor) {
+        this.mAuthor = mAuthor;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    private String mLocation;
+    private String mCallNumber;
+
+
+
+}
---
+++ app/src/main/java/io/github/lingnanlu/hustlibrary/MainActivity.java
@@ -0,0 +1,44 @@
+package io.github.lingnanlu.hustlibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity
+        extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner)findViewById(R.id.catagorySpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                .createFromResource(this, R.array.category, android
+                        .R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
