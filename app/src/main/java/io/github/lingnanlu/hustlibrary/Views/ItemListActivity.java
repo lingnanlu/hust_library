79bea6a4525dbf6c035d32b9d4eb4e4555ddb948
lingnanlu <lingnanlu@gmail.com>
2015-12-22 22:26:31 +0800

    1. 利用自定义的逻辑解决异步加载图片重复问题 2. 现在异步任务过多，加载缓慢

--- app/src/main/java/io/github/lingnanlu/hustlibrary/Views/ItemListActivity.java
+++ app/src/main/java/io/github/lingnanlu/hustlibrary/Views/ItemListActivity.java
@@ -274,7 +274,7 @@ public class ItemListActivity extends AppCompatActivity implements
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            Log.d(TAG, "position : " + position );
+           // Log.d(TAG, "position : " + position );
             ViewHolder viewHolder;
 
             if (convertView == null) {
@@ -304,8 +304,8 @@ public class ItemListActivity extends AppCompatActivity implements
 
             Item item = mBookItems.get(position);
 
-            Log.d(TAG, "position : " + position + " imgUrl " + item
-                    .getImageUrl());
+           // Log.d(TAG, "position : " + position + " imgUrl " + item
+            //        .getImageUrl());
             viewHolder.bookAuthor.setText(item.getAuthor());
             viewHolder.bookPress.setText(item.getPress());
             viewHolder.bookTitle.setText(item.getBookTitle());
@@ -317,49 +317,61 @@ public class ItemListActivity extends AppCompatActivity implements
 
             //有的item没有封面imgeUrl会是/screen/xxxxx
             if(item.getImageUrl().startsWith("http")) {
-                if (mCachedBitmap.containsKey(item.getImageUrl())) {
+//                if (mCachedBitmap.containsKey(item.getImageUrl())) {
+//
+//                    viewHolder.bookCover.setImageBitmap(mCachedBitmap.get(item.getImageUrl()));
+//
+//                } else {
+//
+//                    loadBookCover(item.getImageUrl(), viewHolder.bookCover);
+//
+//                }
 
-                    viewHolder.bookCover.setImageBitmap(mCachedBitmap.get(item.getImageUrl()));
+                String identifer = item.getImageUrl();
+                Log.d(TAG, "position : " + position + "\nidentifer " +
+                        identifer + "\n" + viewHolder.bookCover +
+                        " " +
+                        "\n" + convertView);
+                viewHolder.bookCover.setTag(identifer);
 
-                } else {
-
-                    loadBookCover(item.getImageUrl(), viewHolder.bookCover);
-
-                }
+                viewHolder.bookCover.setImageBitmap(mPlaceHolderBitmap);
+                BookCoverDownloaderTask task = new
+                        BookCoverDownloaderTask(identifer, viewHolder.bookCover);
+                task.execute(item.getImageUrl());
             }
 
             return convertView;
         }
 
-        private void loadBookCover(String imgUrl, ImageView imageView) {
-
-            if(cancelPotentialWork(imageView) ) {
-
-                final BookCoverDownloaderTask task = new BookCoverDownloaderTask
-                        (imageView);
-
-                final AsyncDrawable asyncDrawable = new AsyncDrawable
-                        (getResources(), mPlaceHolderBitmap, task);
-
-                imageView.setImageDrawable(asyncDrawable);
-
-                task.execute(imgUrl);
-            }
-
-        }
-        private boolean cancelPotentialWork(ImageView imageView) {
-
-            final BookCoverDownloaderTask task = getBookCoverDownloaderTask
-                    (imageView);
-
-            if (task != null) {
-
-                task.cancel(true);
-
-            }
-
-            return true;
-        }
+//        private void loadBookCover(String imgUrl, ImageView imageView) {
+//
+//            if(cancelPotentialWork(imageView) ) {
+//
+//                final BookCoverDownloaderTask task = new BookCoverDownloaderTask
+//                        (imageView);
+//
+//                final AsyncDrawable asyncDrawable = new AsyncDrawable
+//                        (getResources(), mPlaceHolderBitmap, task);
+//
+//                imageView.setImageDrawable(asyncDrawable);
+//
+//                task.execute(imgUrl);
+//            }
+//
+//        }
+//        private boolean cancelPotentialWork(ImageView imageView) {
+//
+//            final BookCoverDownloaderTask task = getBookCoverDownloaderTask
+//                    (imageView);
+//
+//            if (task != null) {
+//
+//                task.cancel(true);
+//
+//            }
+//
+//            return true;
+//        }
 
 
         class ViewHolder {
@@ -377,46 +389,24 @@ public class ItemListActivity extends AppCompatActivity implements
             Void,
             Bitmap> {
 
-        private final WeakReference<ImageView> imageViewWeakReference;
+        String mIdentifer;
+        ImageView mImageView;
+//        private final WeakReference<ImageView> imageViewWeakReference;
 
-        public BookCoverDownloaderTask(ImageView imageView) {
+        public BookCoverDownloaderTask(String identifer, ImageView
+                                       imageView) {
 
-            imageViewWeakReference = new WeakReference<ImageView>(imageView);
+            mIdentifer = identifer;
+            mImageView = imageView;
+//            imageViewWeakReference = new WeakReference<ImageView>(imageView);
 
         }
 
         @Override
         protected Bitmap doInBackground(String... params) {
 
-            return downloadBookCover(params[0]);
-
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-
-            if (isCancelled()) {
-                bitmap = null;
-            }
-
-            if (imageViewWeakReference != null && bitmap != null) {
-
-                final ImageView imageView = imageViewWeakReference.get();
-                final BookCoverDownloaderTask bookCoverDownloaderTask =
-                        getBookCoverDownloaderTask(imageView);
-
-                if (imageView != null && this == bookCoverDownloaderTask) {
-                    imageView.setImageBitmap(bitmap);
-                }
-            }
-
-        }
-
-        private Bitmap downloadBookCover(String imageUrl) {
-
-
+            String imageUrl = params[0];
             Request request = new Request.Builder().url(imageUrl).build();
-
 
             try {
 
@@ -427,9 +417,9 @@ public class ItemListActivity extends AppCompatActivity implements
                 if (in != null) {
                     Bitmap bookCover = BitmapFactory.decodeStream(in);
 
-                    if(!mCachedBitmap.containsKey(imageUrl)) {
-                        mCachedBitmap.put(imageUrl, bookCover);
-                    }
+//                    if(!mCachedBitmap.containsKey(imageUrl)) {
+//                        mCachedBitmap.put(imageUrl, bookCover);
+//                    }
                     return bookCover;
                 }
 
@@ -438,10 +428,259 @@ public class ItemListActivity extends AppCompatActivity implements
             }
 
             return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+
+            String identifer = (String)mImageView.getTag();
+
+            Log.d(TAG, "onPostExecute mIdentifer " + mIdentifer +
+                    "\n" +
+                    "identifer " + identifer);
+
+            //这里有个问题，有可能有多个任务为一个imageView加载图片，如果一个是正确的，如何取消其它的？
+            //产生这个问题的原因是有多个任务为一个imageView加载图片，而imageView
+            // 并不知道有多少个worker为其服务，所以无法取消特定的worker.
+            // 不用取消，如果不是为其加载的work，即identifer不同，则什么都不做就行了
+            // 如果使用一个worker，通过队列的方式来做就没问题了，因为其处理流程是从队列中取消息，虽然会有多个消息为
+            // 同一个imageView服务，但是如果执行该消息时发现tag不符合，什么也不做就可以了。
+            if(mIdentifer.equals(identifer)) {
+                mImageView.setImageBitmap(bitmap);
+            }
+
+//            if (isCancelled()) {
+//                bitmap = null;
+//            }
+//
+//            if (imageViewWeakReference != null && bitmap != null) {
+//
+//                final ImageView imageView = imageViewWeakReference.get();
+//                final BookCoverDownloaderTask bookCoverDownloaderTask =
+//                        getBookCoverDownloaderTask(imageView);
+//
+//                if (imageView != null && this == bookCoverDownloaderTask) {
+//                    imageView.setImageBitmap(bitmap);
+//                }
+//            }
+
         }
 
 
+
     }
+
+//    private class ItemAdapter extends BaseAdapter {
+//
+//        private LayoutInflater inflater;
+//
+//        public ItemAdapter(Context context) {
+//
+//            inflater = LayoutInflater.from(context);
+//
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            if(mBookItems != null) {
+//
+//                return mBookItems.size();
+//            }
+//
+//            return 0;
+//
+//        }
+//
+//        @Override
+//        public Object getItem(int position) {
+//
+//            if(mBookItems != null) {
+//
+//                return mBookItems.get(position);
+//
+//            }
+//
+//            return null;
+//
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            return position;
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//
+//            Log.d(TAG, "position : " + position );
+//            ViewHolder viewHolder;
+//
+//            if (convertView == null) {
+//
+//                convertView = inflater.inflate(R.layout
+//                                .list_item,
+//                        null);
+//
+//                viewHolder = new ViewHolder();
+//
+//                viewHolder.bookAuthor = (TextView) convertView.findViewById(R
+//                        .id.bookAuthor);
+//                viewHolder.bookTitle = (TextView) convertView.findViewById(R
+//                        .id.bookTitle);
+//                viewHolder.bookPress = (TextView) convertView.findViewById(R
+//                        .id.bookPress);
+//                viewHolder.bookCover = (ImageView) convertView.findViewById(R
+//                        .id.bookCover);
+//
+//                convertView.setTag(viewHolder);
+//
+//            } else {
+//
+//                viewHolder = (ViewHolder)convertView.getTag();
+//
+//            }
+//
+//            Item item = mBookItems.get(position);
+//
+//            Log.d(TAG, "position : " + position + " imgUrl " + item
+//                    .getImageUrl());
+//            viewHolder.bookAuthor.setText(item.getAuthor());
+//            viewHolder.bookPress.setText(item.getPress());
+//            viewHolder.bookTitle.setText(item.getBookTitle());
+//
+//
+////            new BookCoverDownloaderTask(viewHolder.bookCover).
+////                    execute(item.getImageUrl());
+//
+//
+//            //有的item没有封面imgeUrl会是/screen/xxxxx
+//            if(item.getImageUrl().startsWith("http")) {
+//                if (mCachedBitmap.containsKey(item.getImageUrl())) {
+//
+//                    viewHolder.bookCover.setImageBitmap(mCachedBitmap.get(item.getImageUrl()));
+//
+//                } else {
+//
+//                    loadBookCover(item.getImageUrl(), viewHolder.bookCover);
+//
+//                }
+//            }
+//
+//            return convertView;
+//        }
+//
+//        private void loadBookCover(String imgUrl, ImageView imageView) {
+//
+//            if(cancelPotentialWork(imageView) ) {
+//
+//                final BookCoverDownloaderTask task = new BookCoverDownloaderTask
+//                        (imageView);
+//
+//                final AsyncDrawable asyncDrawable = new AsyncDrawable
+//                        (getResources(), mPlaceHolderBitmap, task);
+//
+//                imageView.setImageDrawable(asyncDrawable);
+//
+//                task.execute(imgUrl);
+//            }
+//
+//        }
+//        private boolean cancelPotentialWork(ImageView imageView) {
+//
+//            final BookCoverDownloaderTask task = getBookCoverDownloaderTask
+//                    (imageView);
+//
+//            if (task != null) {
+//
+//                task.cancel(true);
+//
+//            }
+//
+//            return true;
+//        }
+//
+//
+//        class ViewHolder {
+//
+//            TextView bookTitle;
+//            TextView bookAuthor;
+//            TextView bookPress;
+//            ImageView bookCover;
+//
+//
+//        }
+//    }
+//
+//    private class BookCoverDownloaderTask extends AsyncTask<String,
+//            Void,
+//            Bitmap> {
+//
+//        private final WeakReference<ImageView> imageViewWeakReference;
+//
+//        public BookCoverDownloaderTask(ImageView imageView) {
+//
+//            imageViewWeakReference = new WeakReference<ImageView>(imageView);
+//
+//        }
+//
+//        @Override
+//        protected Bitmap doInBackground(String... params) {
+//
+//            return downloadBookCover(params[0]);
+//
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Bitmap bitmap) {
+//
+//            if (isCancelled()) {
+//                bitmap = null;
+//            }
+//
+//            if (imageViewWeakReference != null && bitmap != null) {
+//
+//                final ImageView imageView = imageViewWeakReference.get();
+//                final BookCoverDownloaderTask bookCoverDownloaderTask =
+//                        getBookCoverDownloaderTask(imageView);
+//
+//                if (imageView != null && this == bookCoverDownloaderTask) {
+//                    imageView.setImageBitmap(bitmap);
+//                }
+//            }
+//
+//        }
+//
+//        private Bitmap downloadBookCover(String imageUrl) {
+//
+//
+//            Request request = new Request.Builder().url(imageUrl).build();
+//
+//
+//            try {
+//
+//                Response response = mClient.newCall(request).execute();
+//
+//                InputStream in = response.body().byteStream();
+//
+//                if (in != null) {
+//                    Bitmap bookCover = BitmapFactory.decodeStream(in);
+//
+//                    if(!mCachedBitmap.containsKey(imageUrl)) {
+//                        mCachedBitmap.put(imageUrl, bookCover);
+//                    }
+//                    return bookCover;
+//                }
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            return null;
+//        }
+//
+//
+//    }
 
     private class ListViewInitTask extends AsyncTask<String, Void,
             Void> {
